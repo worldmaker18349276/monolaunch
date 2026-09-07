0579fa5d21cc56cd0379cb5c6af31a0df69ec1a2
worldmaker <worldmaker18349276@gmail.com>
2026-09-07 20:53:46 +0800

    simplify with_roscore.py usage

--- scripts/with_roscore.py
+++ scripts/with_roscore.py
@@ -6,13 +6,12 @@ import subprocess
 import signal
 import sys
 import os
-import time
-from typing import Optional
+from typing import Any, Tuple
 from monolaunch.monoresource import Machine
 import xml.etree.ElementTree as ET
 
 
-def _get_master_machine(launch_file: Path) -> Optional[Machine]:
+def _get_local_and_master_machine(launch_file: Path) -> Tuple[Machine, Machine]:
     root = ET.parse(launch_file).getroot()
 
     machines = {
@@ -20,45 +19,63 @@ def _get_master_machine(launch_file: Path) -> Optional[Machine]:
         for machine in root.findall("machine")
     }
 
-    for node in root.findall("master"):
-        machine_name = node.get("machine")
-        assert machine_name is not None
+    def get_machine(machine_name: str) -> Machine:
         machine_tag = machines.get(machine_name)
-        assert machine_tag is not None
+        if machine_tag is None:
+            raise RuntimeError(f"[with_roscore] cannot find machine tag with name {machine_name!r}")
         user = machine_tag.get("user", "")
         password = machine_tag.get("password", "")
         address = machine_tag.get("address", "")
         env_loader = machine_tag.get("env-loader", "")
         return Machine(user=user, password=password, address=address, env_loader=tuple(shlex.split(env_loader)))
 
-    return None
+    local = get_machine("local")
+    if not local.is_local():
+        raise RuntimeError("[with_roscore] local machine is not local")
+
+    master = None
+    for node in root.findall("master"):
+        machine_name = node.get("machine")
+        if machine_name is None:
+            raise RuntimeError("[with_roscore] cannot find machine attr in the master tag")
+        master = get_machine(machine_name)
+        break
+
+    if master is None:
+        raise RuntimeError("[with_roscore] cannot find master tag")
+
+    return local, master
 
 def main():
     command = sys.argv[1:]
     if len(command) < 2 or command[0] != "roslaunch" or not command[1].endswith(".launch"):
         raise ValueError("with_roscore.py must be prefixed before `roslaunch <launch_file.launch> ...`")
-    machine = _get_master_machine(Path(command[1])) or Machine()
+    command[2:2] = ["--wait"] # force to wait my roscore
+    local, master = _get_local_and_master_machine(Path(command[1]))
 
-    ros_master_uri = f"http://{machine.address}:11311"
+    ros_master_uri = f"http://{master.address}:11311"
     os.environ["ROS_MASTER_URI"] = ros_master_uri
 
-    ssh = subprocess.Popen(machine.command(["roscore"]))
+    roscore = master.command(["roscore"])
+    print("[with_roscore] run roscore", roscore)
+    roscore_proc = subprocess.Popen(roscore)
+    # TODO: exit if roscore is dead
 
-    def cleanup(*_):
-        ssh.terminate()
+    def cleanup(*_: Any):
+        print("[with_roscore] cleanup roscore...")
+        roscore_proc.terminate()
         try:
-            ssh.wait(timeout=5)
+            roscore_proc.wait(timeout=5)
         except subprocess.TimeoutExpired:
-            ssh.kill()
-            ssh.wait()
+            roscore_proc.kill()
+            roscore_proc.wait()
 
     for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGHUP):
         signal.signal(sig, cleanup)
 
     try:
-        # Give roscore time to start.
-        time.sleep(1)
-
+        command = local.command(command)
+        print("[with_roscore] run roslaunch", command)
         result = subprocess.run(command)
 
     finally:
